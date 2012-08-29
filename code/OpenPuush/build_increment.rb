481d4936e110c52567b2b3216d4d8a19bd0a461c
hmbl9r <hmbl9r@gmail.com>
2012-08-29 16:24:43 -0700

    Update code/OpenPuush/build_increment.rb

--- code/OpenPuush/build_increment.rb
+++ code/OpenPuush/build_increment.rb
@@ -1,10 +1,10 @@
 unless File.exists? 'build_num'
-	open('build_num', 'w') { |f| f.print 0 }
+  open('build_num', 'w') { |f| f.print 0 }
 end
 
 open('build_num', 'r+') do |f|
-	c = f.read.to_i + 1
-	f.seek 0
+  c = f.read.to_i + 1
+  f.seek 0
   f.print c
   print c
 end
